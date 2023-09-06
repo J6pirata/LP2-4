3b000703fc8afd5039f99d48fd218c0f46e7dc00
J6pirata <74542025+J6pirata@users.noreply.github.com>
2023-09-06 07:18:06 -0300

    Add files via upload

---
+++ Tela.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.
+
+ public class Tela extends Frame {
+    private Button b1 = new Button("ok");
+    private Button b2 = new Button("limpar ");
+    private Button b3 = new Button("Mostra");
+    private Button b4 = new Button("Sair");
+
+    public tela(String Titulo, titulo, int largura, int altura, int colinic, int
+                lininic){
+            super(titulo);
+
+
+    }
+ }
