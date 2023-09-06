07b2fbd1ac3fbb256f59371dd6d799bbd679f214
J6pirata <74542025+J6pirata@users.noreply.github.com>
2023-09-06 07:14:49 -0300

    Add files via upload

---
+++ TesteAuthor.java
@@ -0,0 +1,12 @@
+package Aulatp1;
+
+public class TesteAuthor {
+
+ public static void main(String[] args) {
+
+
+        Author autor1 = new Author("Rafael", " " , 'm');
+        setEmail("rafael123@gmail.com");
+        toString(autor1);
+    }
+}
